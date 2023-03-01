11d75169b73fcf7faf31700d6f4e5779cd6bfaf8
Aleksei <dulittlefamily@yandex.ru>
2023-03-01 23:00:06 +0300

    Add function of create array

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющего массива строк формирует массив из строк, длина которых меньше либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+void CreateNewArray(string[] array)
+{    
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Input {i + 1} element of array: ");
+        string temp = new string(Console.ReadLine());
+        if (String.IsNullOrWhiteSpace(temp))
+        {
+            Console.WriteLine("Value is empty or null, please, re-input");
+            i -= 1;
+        }
+        else array[i] = temp;
+    }
+}
