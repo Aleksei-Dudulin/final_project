4df89ae1c17d7b9fe940f08387199b040f1768c5
Aleksei <dulittlefamily@yandex.ru>
2023-03-02 01:29:19 +0300

    Add task decision v.1

--- Project/Program.cs
+++ Project/Program.cs
@@ -2,7 +2,7 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-void CreateNewArray(string[] array)
+void CreateArray(string[] array)
 {    
     for(int i = 0; i < array.Length; i++)
     {
@@ -27,3 +27,45 @@ void ShowArray(string[] array)
             Console.Write($"\"{array[i]}\", ");
     Console.WriteLine("]");
 }
+
+string[] CreateNewArray(string[] array)
+{
+    int count = 0;
+    string[] newArray = new string[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        char[] splitToChars = array[i].ToCharArray();
+        if(splitToChars.Length <= 3)
+        if(array[i].Length <= 3) 
+        {
+            newArray[count]= array[i];
+            count++;
+        }
+    }
+    if(array.Length != count)    
+        Array.Resize(ref newArray, count);
+    return newArray;
+}
+/*
+void RunProgram()
+{
+    int n = Convert.ToInt32(Console.ReadLine());    
+    Console.WriteLine();
+    string[] myArray = new string[n];
+    CreateArray(myArray);
+    Console.Write("\nYour input array: ");
+    ShowArray(myArray);
+    Console.Write("Array, after program runing: ");
+    ShowArray(CreateNewArray(myArray));
+}
+*/
+Console.Write("Input quantity strings of array: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if(n<0) n*=(-1);
+string[] myArray = new string[n];
+CreateArray(myArray);
+Console.Write("\nYour input array: ");
+ShowArray(myArray);
+Console.Write("Array, after program runing: ");
+ShowArray(CreateNewArray(myArray));
